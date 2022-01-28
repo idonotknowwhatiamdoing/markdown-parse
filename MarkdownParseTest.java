bfe7abee1421bcb19304fc2b560400433a221c66
idonotknowwhatiamdoing <50209503+idonotknowwhatiamdoing@users.noreply.github.com>
2022-01-27 22:38:32 -0800

    update to paths

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -16,16 +16,39 @@ public class MarkdownParseTest {
     @Test
     public void testFile1() throws IOException
     {
-        String contents = Files.readString(Path.of("./test-file-1.md"));
+        String contents = Files.readString(Path.of("C:/Users/zhong/Desktop/markdown-parse-wk2/test-file-1.md"));
         ArrayList<String> testList = new ArrayList<>();
         assertEquals(testList, MarkdownParse.getLinks(contents));
     }
 
     @Test
-    public void testFile1() throws IOException {
-        String contents= Files.readString(Path.of("./test-file.md"));
+    public void testFile2() throws IOException {
+        String contents= Files.readString(Path.of("C:/Users/zhong/Desktop/markdown-parse-wk2/test-file.md"));
         List<String> expect = List.of("https://something.com", "some-page.html");
         assertEquals(MarkdownParse.getLinks(contents), expect);
     }
     
+    @Test
+    public void testEmptyParentheses() throws IOException
+    {
+        String contents = Files.readString(Path.of("C:/Users/zhong/Desktop/markdown-parse-wk2/test-issue-1.md"));
+        List<String> expect = List.of();
+        assertEquals(MarkdownParse.getLinks(contents), expect);
+    }
+
+    @Test
+    public void testIncompleteParentheses()  throws IOException
+    {
+        String contents = Files.readString(Path.of("C:/Users/zhong/Desktop/markdown-parse-wk2/test-issue-2.md"));
+        List<String> expect = List.of();
+        assertEquals(MarkdownParse.getLinks(contents), expect);
+    }
+
+    @Test
+    public void testLinkWithTextBetween()  throws IOException
+    {
+        String contents = Files.readString(Path.of("C:/Users/zhong/Desktop/markdown-parse-wk2/test-issue-3.md"));
+        List<String> expect = List.of();
+        assertEquals(MarkdownParse.getLinks(contents), expect);
+    }
 }
