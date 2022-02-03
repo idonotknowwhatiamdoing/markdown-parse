5a883a19fde8c2081c44109e21b0844215f69774
idonotknowwhatiamdoing <50209503+idonotknowwhatiamdoing@users.noreply.github.com>
2022-02-03 11:51:53 -0800

    new commit

---
+++ MarkdownParseTestNew.java
@@ -0,0 +1,33 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+
+public class MarkdownParseTestNew {
+    @Test
+    public void addition() {
+        assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void noBrackets() throws IOException {
+        String contents = Files.readString(Path.of("./test-issue-1.md"));
+        assertEquals(new ArrayList<>(), MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void noParentheses() throws IOException {
+        String contents = Files.readString(Path.of("./test-issue-2.md"));
+        assertEquals(new ArrayList<>(), MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void textBetween() throws IOException {
+        String contents = Files.readString(Path.of("./test-issue-3.md"));
+        assertEquals(new ArrayList<>(), MarkdownParse.getLinks(contents));
+    }
+
+}
